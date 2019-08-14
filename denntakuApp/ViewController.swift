7855f98b239fecf6a5d077379a1f0e5fda4aa2d2
“yuriko0922” <yrk922@gmail.com>
2019-08-14 20:19:20 +0900

    処理書きまくった

--- denntakuApp/ViewController.swift
+++ denntakuApp/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController {
     
     //定義系を書きます
     //画面に表示されている文字
-    var nowNumber: Double = 0;
+    var nowNumber: Double = 0
     //さっきまで表示されてた文字
-    var beforNum: Double = 0;
+    var beforNum: Double = 0
     //計算してOKかの精査
     var okNum = false
     //数入力されたかの精査
     var putNum = false
     //記号たち
-    var kigou = 0;
+    var kigou = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
     //数字ボタン紐ずけ
     @IBAction func numberBottun(_ sender: UIButton) {
         if okNum == true{
-            kekkaLabel.text = String(sender.tag-1)
+            kekkaLabel.text = String(sender.tag)
             nowNumber = Double(kekkaLabel.text!)!
             okNum = false
         } else {
@@ -47,6 +47,35 @@ class ViewController: UIViewController {
     
     //記号系ボタン紐ずけ
     @IBAction func kigouBottun(_ sender: UIButton) {
+        if kekkaLabel.text != "" && sender.tag != 11 && sender.tag != 16{
+            if putNum {
+            beforNum = Double(kekkaLabel.text!)!
+            }
+            if sender.tag == 12{
+                kekkaLabel.text = "÷"
+            } else if sender.tag == 13 {
+                kekkaLabel.text = "×";
+            } else if sender.tag == 14 {
+                kekkaLabel.text = "-";
+            } else if sender.tag == 15 {
+                kekkaLabel.text = "＋"
+            }
+            kigou = sender.tag
+            okNum = true
+            //＝ボタン押した時の処理
+        } else if sender.tag == 16 {
+            if kigou == 12 {
+                kekkaLabel.text = String(beforNum / nowNumber)
+            } else if kigou == 13 {
+             kekkaLabel.text = String(beforNum * nowNumber)
+            } else if kigou == 14 {
+                 kekkaLabel.text = String(beforNum - nowNumber)
+            } else if kigou == 15 {
+                 kekkaLabel.text = String(beforNum + nowNumber)
+            }
+            //クリアボタン押された時の処理
+            
+        }
         
         
         
